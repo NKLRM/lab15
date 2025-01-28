28e95ea7d9f43b786f7b083ed6d2827ae8eaa2ee
NKLRM <nekoloveramenxd@gmail.com>
2025-01-28 15:12:57 +0700

    lab15_4.cpp complete

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,16 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle(int &a, int &b, int &c, int &d) {
+    int x[] = {a, b, c, d};
+    
+    for (int i = 3; i > 0; i--) {
+        int j = rand()%i;
+        swap(x[i], x[j]);
+    }
+    a = x[0];
+    b = x[1];
+    c = x[2];
+    d = x[3];
+}
